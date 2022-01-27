b6a1a524c37089c3055ab072155c1d7d23ccf6bb
Andrew Barba <barba@hey.com>
2022-01-27 00:48:38 -0500

    Create vpc

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import * as pulumi from '@pulumi/pulumi'
-import * as aws from '@pulumi/aws'
 import * as awsx from '@pulumi/awsx'
+
+// Allocate a new VPC with the default settings:
+const vpc = new awsx.ec2.Vpc('swift-cloud', {})
 
 // Create ECR repo
 const repo = new awsx.ecr.Repository('swift-cloud')
@@ -11,6 +13,7 @@ const image = repo.buildAndPushImage('./')
 // Create container
 const container = new awsx.ecs.FargateService('swift-build', {
   taskDefinitionArgs: {
+    vpc,
     container: {
       image
     }
